3ba7d81000038e9f1fc864786977ef527f7cb727
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-01-19 12:28:18 -0500

    Progressed the tutorial Added docstrings to all functions https://classroom.udacity.com/nanodegrees/nd803/parts/686861db-d0bf-4fe2-ab57-287444412d3f/modules/434cf327-f998-4e1d-8d7a-04ed96c43872/lessons/8393b87e-4cd4-49ff-b0bc-50545990c927/concepts/42871286030923

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,4 +1,8 @@
 //Template Code: https://gist.github.com/udacityandroid/83242daf8a43b743d05e98733a35b19f
+//Template Code:  https://gist.github.com/anonymous/fa134c55a4a43e8d6004
+
+//How to write documentation comments: https://www.oracle.com/technetwork/articles/java/index-137868.html
+
 package com.example.android.justjava;
 
 import android.os.Bundle;
@@ -6,9 +10,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-/**
- * This app displays an order form to order coffee.
- */
+import java.text.NumberFormat;
+
+/** Allows the user to order drinks. */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,15 +21,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /** Updates the screen with the current order.
+     *
+     *@see  MainActivity.displayPrice
+     *@see  MainActivity.display
+     *
+     * Use For: onClick
+     */
     public void submitOrder(View view) {
-        display(1);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
+    /** Displays the number of drinks to order on the app screen.
+     *
+     *@param    number - How many drinks to order
+     *@see      MainActivity.displayPrice
+     *
+     * Example Input: display(2);
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /** Displays the given price on the app screen.
+     *
+     *@param    number - The price to display
+     *@see      MainActivity.display
+     *
+     * Example Input: displayPrice(2 * 5);
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
